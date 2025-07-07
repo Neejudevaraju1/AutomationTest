bc1c36f18e9af9a61e46d1a48534a9d1f3e76723
Neeju Devaraju <Neeju Devaraju@DESKTOP-N3G62FT>
2025-07-07 16:30:59 +0530

    changes

--- new/src/main/java/automation/UnitManagement.java
+++ new/src/main/java/automation/UnitManagement.java
@@ -1,7 +1,14 @@
 package automation;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.By.ByClassName;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class UnitManagement extends Base {
 	
@@ -10,12 +17,31 @@ public class UnitManagement extends Base {
 		module.click();
 		System.out.println(driver.getTitle());
 	}
+	public void permissionNOC() {
+		driver.navigate().to("https://rise-testing.kran.co.in/unitmanagement/permission/gotoUnitPermission");
+		WebElement unitselect=driver.findElement(By.xpath("//select[@id='unitSetupListId']"));
+		Select unit= new Select(unitselect);
+		unit.selectByIndex(1);
+		WebElement serviceName=driver.findElement(By.xpath("//select[@id='serviceListId']"));
+		Select service=new Select(serviceName);
+		service.selectByIndex(1);
+		WebElement approve=driver.findElement(By.xpath("//button[@id='saveButton']"));
+		approve.click();
+		WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+		WebElement permissionFor=driver.findElement(By.id("select2-serviceId-container"));
+		permissionFor.click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("select2-selection__arrow")));
+		
+		Select permission=new Select(permissionFor);
+		permission.selectByIndex(4);
+	}
 
 	public static void main(String[] args) throws InterruptedException {
 		UnitManagement unit=new UnitManagement();
 		unit.browser();
 		unit.locator();
 		unit.unitManagement();
+		unit.permissionNOC();
 
 	}
 
