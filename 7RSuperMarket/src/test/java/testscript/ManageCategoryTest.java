4b75874e6952dbf5710262bbdc56785423193307
Neeju Devaraju <Neeju Devaraju@DESKTOP-N3G62FT>
2025-07-26 16:24:18 +0530

    spaces

--- 7RSuperMarket/src/test/java/testscript/ManageCategoryTest.java
+++ 7RSuperMarket/src/test/java/testscript/ManageCategoryTest.java
@@ -1,9 +1,7 @@
 package testscript;
 
 import java.io.IOException;
-
 import org.testng.annotations.Test;
-
 import pages.HomePage;
 import pages.LoginPage;
 import pages.ManageCategory;
@@ -16,11 +14,11 @@ public class ManageCategoryTest extends Base {
   public void verifyThatTheUserIsAbleToAddNewCategory() throws IOException{
 	LoginPage login=new LoginPage(driver);	  
 	String username=ExcelUtility.readStringData(1, 0, "Login_Page");
-			String password=ExcelUtility.readStringData(1, 1, "Login_Page");
-		  login.enterUsernameAndPassword(username, password);
-		  homepage=login.clickSiginButton();
-		  managecategory=homepage.clickMoreInfoCategory();
-		  String categoryname=ExcelUtility.readStringData(1, 0, "Manage_Category");
-		  managecategory.clickNewCategoryButton().enterNewCategory(categoryname).selectGroups().chooseFile().clickOnSaveButton();
+	String password=ExcelUtility.readStringData(1, 1, "Login_Page");
+	login.enterUsernameAndPassword(username, password);
+    homepage=login.clickSiginButton();
+	managecategory=homepage.clickMoreInfoCategory();
+	String categoryname=ExcelUtility.readStringData(1, 0, "Manage_Category");
+	managecategory.clickNewCategoryButton().enterNewCategory(categoryname).selectGroups().chooseFile().clickOnSaveButton();
   }
 }
