00eef0d6931e002a1f13eaa441d96f6c7dc2434a
Neeju Devaraju <Neeju Devaraju@DESKTOP-N3G62FT>
2025-08-26 12:53:12 +0530

    change

--- Seepz/src/main/java/pages/AddAgenda.java
+++ Seepz/src/main/java/pages/AddAgenda.java
@@ -1,5 +1,18 @@
 package pages;
 
-public class AddAgenda {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+public class AddAgenda {
+WebDriver driver;
+public AddAgenda(WebDriver driver) {
+	this.driver=driver;
+	PageFactory.initElements(driver, this);
+}
+@FindBy(xpath="//a[contains (@class,'badge badge-status  bg-primary-soft')]")WebElement createAgenda;
+@FindBy(xpath="//select[@id='agendaId']")WebElement agendaTitle;
+@FindBy(xpath="//select[@id='unitTypeId']") WebElement sezName;
+@FindBy(xpath="//input[@id='refernceNo']")WebElement sequenceNumber;
 }
